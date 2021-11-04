2e1e3cb609a24bcffe344eb831c0c8152e6126ec
omid astaraki <omidocean@gmail.com>
2021-11-04 11:10:43 +0330

    chore: update typescript config files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withPlugins = require('next-compose-plugins');
-const withSourceMaps = require('@zeit/next-source-maps');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 const { i18n } = require('./next-i18next.config');
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   i18n,
   future: {
@@ -15,4 +17,4 @@ const nextConfig = {
   experimental: { optimizeCss: true },
 };
 
-module.exports = withPlugins([withBundleAnalyzer, withSourceMaps], nextConfig);
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
