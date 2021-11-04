221caa7e46aa32ba7187726c1c0c9800a4f12ee3
omid astaraki <omidocean@gmail.com>
2021-11-04 21:21:50 +0330

    chore: update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
-module.exports = {
+/** @type {import('postcss').Postcss} */
+const postCssConfig = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: `no-2009`,
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
   },
 };
+
+module.exports = postCssConfig;
