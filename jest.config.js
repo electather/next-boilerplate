df6b0e8b23a5bb44d27404c23c92399b03099575
omid astaraki <omidocean@gmail.com>
2021-11-04 12:26:49 +0330

    chore: enforce backtick for strings

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/test-setup/setupTests.js'],
-  testMatch: ['**/__tests__/*.ts?(x)'],
+  testPathIgnorePatterns: [`<rootDir>/.next/`, `<rootDir>/node_modules/`],
+  setupFilesAfterEnv: [`<rootDir>/test-setup/setupTests.js`],
+  testMatch: [`**/__tests__/*.ts?(x)`],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': [
-      'babel-jest',
-      { configFile: './test-setup/babel.config.js' },
+      `babel-jest`,
+      { configFile: `./test-setup/babel.config.js` },
     ],
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/styles/__mocks__/styleMock.js',
-    '^@pages/(.*)$': '<rootDir>/pages/$1',
-    '^@pages': '<rootDir>/pages',
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': `<rootDir>/styles/__mocks__/styleMock.js`,
+    '^@pages/(.*)$': `<rootDir>/pages/$1`,
+    '^@/(.*)$': `<rootDir>/src/$1`,
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: `jsdom`,
 };
